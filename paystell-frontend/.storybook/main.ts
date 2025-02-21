a2dbd2375f71891bc96236532d444b592f9fc04e
JoelVR17 <vermudo.com@gmail.com>
2025-02-20 23:34:21 -0600

    feat: update Storybook configuration to enhance framework options and add new addons

--- paystell-frontend/.storybook/main.ts
+++ paystell-frontend/.storybook/main.ts
@@ -2,23 +2,18 @@ import { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  framework: "@storybook/nextjs",
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
   addons: [
     "@storybook/addon-links",
+    "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-postcss",
+    "@chromatic-com/storybook",
   ],
-  webpackFinal: async (config) => {
-    if (config.module && config.module.rules) {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      });
-    }
-
-    return config;
-  },
 };
 
 export default config;
