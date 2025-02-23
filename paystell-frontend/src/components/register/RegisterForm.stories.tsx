458c719a6b40813af2a48f6f81ab6f88f0786aac
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-02-23 01:01:30 +0000

    feat: add Storybook story for RegisterForm component

---
+++ paystell-frontend/src/components/register/RegisterForm.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryFn } from "@storybook/react";
+import RegisterForm from "./RegisterForm";
+
+export default {
+  title: "Components/RegisterForm",
+  component: RegisterForm,
+} as Meta;
+
+const Template: StoryFn = (args) => <RegisterForm {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
