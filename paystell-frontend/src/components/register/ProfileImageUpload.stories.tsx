eed347505d750bb2e14672181a6290af397d46f3
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-02-23 01:01:25 +0000

    feat: add Storybook story for ProfileImageUpload component

---
+++ paystell-frontend/src/components/register/ProfileImageUpload.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryFn } from "@storybook/react";
+import { useState } from "react";
+import ProfileImageUpload from "./ProfileImageUpload";
+
+export default {
+  title: "Components/ProfileImageUpload",
+  component: ProfileImageUpload,
+} as Meta;
+
+const Template: StoryFn = (args) => {
+  const [preview, setPreview] = useState<string | undefined>(args.previewImage);
+  
+  const handleImageUpload = (file: File | null) => {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => setPreview(reader.result as string);
+      reader.readAsDataURL(file);
+    } else {
+      setPreview(undefined);
+    }
+  };
+
+  return <ProfileImageUpload {...args} previewImage={preview} onImageUpload={handleImageUpload} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  previewImage: undefined,
+};
