86c2eb3db2ec7310d577c9594c36bdb242ead8f4
Renzo Espinosa <renzoespinosa.b@gmail.com>
2025-03-18 01:31:40 +0000

    feat: add QRCode component story

---
+++ paystell-frontend/src/components/TwoFactorAuth/QRCode.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { QRCodeDisplay, QRCodeDisplayProps } from './QRCodeDisplay';
+
+export default {
+    title: 'Components/TwoFactorAuth/QRCodeDisplay',
+    component: QRCodeDisplay,
+} as Meta;
+
+const Template: StoryFn<QRCodeDisplayProps> = (args) => <QRCodeDisplay {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+    otpAuthUrl: 'otpauth://totp/Example:alice@google.com?secret=JBSWY3DPEHPK3PXP&issuer=Example',
+    secret: 'JBSWY3DPEHPK3PXP',
+};
