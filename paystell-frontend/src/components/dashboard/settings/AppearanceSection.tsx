81aab0784e864d8db64d456c3f34887ea981370e
brendamareco <brendaamareco@gmail.com>
2025-03-22 19:22:40 -0300

    Updated settings section

--- paystell-frontend/src/components/dashboard/settings/AppearanceSection.tsx
+++ paystell-frontend/src/components/dashboard/settings/AppearanceSection.tsx
@@ -2,7 +2,7 @@ import { ThemeToggle } from "@/components/ThemeToggle/ThemeToggle";
 
 const AppearanceSection = () => {
   return (
-    <div className="bg-white rounded-lg w-full p-8 mt-8">
+    <div className="bg-card rounded-lg w-full p-8 mt-8">
       <h2 className="text-xl font-semibold mb-6">Appearance</h2>
       <ThemeToggle />
     </div>
