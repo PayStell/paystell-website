ebf0d5d2b1bf36a24309beba569396ca85bd1130
brendamareco <brendaamareco@gmail.com>
2025-03-22 14:48:38 -0300

    Updated input and text area

--- paystell-frontend/src/components/ui/input.tsx
+++ paystell-frontend/src/components/ui/input.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const inputVariants = cva(
-  "flex w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:border-primary focus-visible:border-2 disabled:cursor-not-allowed disabled:opacity-50 my-1",
+  "flex w-full rounded-md border bg-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-input-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:border-primary focus-visible:border-2 disabled:cursor-not-allowed disabled:opacity-50 my-1",
   {
     variants: {
       size: {
@@ -21,7 +21,7 @@ const inputVariants = cva(
       size: "default",
       variant: "default",
     },
-  },
+  }
 );
 
 export interface InputProps
@@ -38,7 +38,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         {...props}
       />
     );
-  },
+  }
 );
 Input.displayName = "Input";
 
