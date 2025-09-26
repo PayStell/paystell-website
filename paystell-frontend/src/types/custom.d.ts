42da6d2b89f9b6de3d267ea35453644301491803
MPSxDev <mparedes.digital@gmail.com>
2025-09-26 14:56:27 -0300

    fix: add types for img

--- paystell-frontend/src/types/custom.d.ts
+++ paystell-frontend/src/types/custom.d.ts
@@ -507,4 +507,40 @@ declare module '@fortawesome/fontawesome-svg-core' {
       string // svgPathData
     ];
   }
+}
+
+// Image file type declarations
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.ico' {
+  const content: string;
+  export default content;
 } 
